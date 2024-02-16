ffa9b7533423f8149dedb06ec4b6f96ede7fde5c
Noah <Dethstroke23@gmail.com>
2024-02-16 04:28:00 +0100

    changing routing nightmare

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./index.css";
 import {
   AboutUs,
@@ -37,150 +37,61 @@ import {
   WorkmanshipFeePage,
 } from "./pages/index";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LandingPage />,
-  },
-  {
-    path: "/home",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <AboutUs />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-  {
-    path: "/our-services",
-    element: <OurServices />,
-  },
-  {
-    path: "/privacy-policy",
-    element: <PrivacyPolicy />,
-  },
-  // ONBOARDING PAGES
-  {
-    path: "/splash",
-    element: <SplashScreen />,
-  },
-  {
-    path: "/phone-number",
-    element: <PhoneNumberPage />,
-  },
-  {
-    path: "/OTP",
-    element: <OTPPage />,
-  },
-  {
-    path: "/name",
-    element: <FullNamePage />,
-  },
-  {
-    path: "/fund-wallet",
-    element: <FundWalletPage />,
-  },
-  {
-    path: "/T&Cs",
-    element: <TermsAndConditions />,
-  },
-  {
-    path: "/location",
-    element: <LocationPage />,
-  },
-  {
-    path: "/manual-location",
-    element: <ManualLocationPage />,
-  },
-  {
-    path: "/upload-image",
-    element: <UploadImagePage />,
-  },
-  {
-    path: "/welcome",
-    element: <WelcomePage />,
-  },
-
-  // BOOKING PAGES
-  {
-    path: "/post",
-    element: <PostRequestPage />,
-  },
-  {
-    path: "/fee",
-    element: <WorkmanshipFeePage />,
-  },
-  {
-    path: "/payment",
-    element: <PaymentPage />,
-  },
-  {
-    path: "/services",
-    element: <AllServices />,
-  },
-  {
-    path: "/created-jobs",
-    element: <CreatedJobs />,
-  },
-  {
-    path: "/job-detail",
-    element: <JobDetail />,
-  },
-  {
-    path: "/aritisian-profile",
-    element: <ArtisianProfile />,
-  },
-  {
-    path: "/images",
-    element: <AllImages />,
-  },
-
-  // INBOX RELATED PAGES
-  {
-    path: "/inbox",
-    element: <InboxPage />,
-  },
-  {
-    path: "/inbox-details",
-    element: <InboxDetailsPage />,
-  },
-
-  // HISTORY RELATED PAGES
-  {
-    path: "/history",
-    element: <HistoryPage />,
-  },
-  {
-    path: "/history-details",
-    element: <HistoryDetailsPage />,
-  },
-
-  // WALLET RELATED PAGES
-  {
-    path: "/wallet",
-    element: <WalletPage />,
-  },
-
-  // USER ACCOUNT RELATED PAGES
-  {
-    path: "/user-account",
-    element: <UserAccountPage />,
-  },
-  {
-    path: "/change-number",
-    element: <ChangePhoneNumberPage />,
-  },
-  {
-    path: "/help",
-    element: <HelpAndSupportPage />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<AboutUs />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/our-services' element={<OurServices />} />
+        <Route path='/privacy-policy' element={<PrivacyPolicy />} />
+
+        {/* ONBOARDING PAGES */}
+
+        <Route path='/splash' element={<SplashScreen />} />
+        <Route path='/phone-number' element={<PhoneNumberPage />} />
+        <Route path='/OTP' element={<OTPPage />} />
+        <Route path='/name' element={<FullNamePage />} />
+        <Route path='/fund-wallet' element={<FundWalletPage />} />
+        <Route path='/T&Cs' element={<TermsAndConditions />} />
+        <Route path='/location' element={<LocationPage />} />
+        <Route path='/manual-location' element={<ManualLocationPage />} />
+        <Route path='/upload-image' element={<UploadImagePage />} />
+        <Route path='/welcome' element={<WelcomePage />} />
+
+        {/* BOOKING PAGES */}
+
+        <Route path='/post' element={<PostRequestPage />} />
+        <Route path='/fee' element={<WorkmanshipFeePage />} />
+        <Route path='/payment' element={<PaymentPage />} />
+        <Route path='/services' element={<AllServices />} />
+        <Route path='/created-jobs' element={<CreatedJobs />} />
+        <Route path='/job-detail' element={<JobDetail />} />
+        <Route path='/aritisian-profile' element={<ArtisianProfile />} />
+        <Route path='/images' element={<AllImages />} />
+
+        {/* INBOX RELATED PAGES */}
+
+        <Route path='/inbox' element={<InboxPage />} />
+        <Route path='/inbox-details' element={<InboxDetailsPage />} />
+
+        {/* HISTORY RELATED PAGES */}
+
+        <Route path='/history' element={<HistoryPage />} />
+        <Route path='/history-details' element={<HistoryDetailsPage />} />
+
+        {/* WALLET RELATED PAGES */}
+
+        <Route path='/wallet' element={<WalletPage />} />
+
+        {/* USER ACCOUNT RELATED PAGES */}
+
+        <Route path='/user-account' element={<UserAccountPage />} />
+        <Route path='/change-number' element={<ChangePhoneNumberPage />} />
+        <Route path='/help' element={<HelpAndSupportPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
