fd0a210ea7d1c2e78a1ae84087f8a25a794bc105
Noah <Dethstroke23@gmail.com>
2024-02-16 04:07:58 +0100

    fund wallet modal

--- src/pages/Wallet/WalletPage.jsx
+++ src/pages/Wallet/WalletPage.jsx
@@ -1,8 +1,21 @@
-import { MobileHeader } from "../../component";
+import { useState } from "react";
+import { Input, MobileHeader, Modal } from "../../component";
 
 export default function WalletPage() {
+  const [fundModal, setFundModal] = useState(false);
+
+  const toggleModal = () => {
+    setFundModal(!fundModal);
+  };
   return (
     <>
+      <Modal title='Input fund amount' state={fundModal} toggle={toggleModal}>
+        <div className='flex flex-col gap-4'>
+          <Input type='text' placeholder='Amount' />
+          <button className='btn btn-block btn-primary'>Next</button>
+          {/* Leads to paystack or whoever's payment modal */}
+        </div>
+      </Modal>
       <MobileHeader />
       <div className='pt-24 pb-12 px-4 flex flex-col gap-8'>
         <h1 className='text-2xl font-bold'>My Wallet</h1>
@@ -11,7 +24,9 @@ export default function WalletPage() {
           <span>Balance</span>
           <span className='text-3xl font-bold'>10,000 NGN</span>
         </div>
-        <button className='btn btn-primary btn-block'>Fund Wallet</button>
+        <button onClick={toggleModal} className='btn btn-primary btn-block'>
+          Fund Wallet
+        </button>
       </div>
     </>
   );
